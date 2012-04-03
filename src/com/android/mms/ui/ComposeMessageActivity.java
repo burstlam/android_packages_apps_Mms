a211ebec40e26fe2910983c63c4b1ca6f337fb4d
gilles le brun <gil.lebrun@orange.com>
2012-04-03 17:03:03 +0200

    Remove message notification while in conversation ui
    While composing message, on data set changed, such as new
    incoming message, remove from notification bar any
    notification regarding the current conversation.
    
    While display list of conversations, on query complete, such
    as new incoming message, remove from notification bar any
    notification regarding the whole list of conversations.
    
    Change-Id: I806d4a921ed13af3db07f1e55b0fc5ba021ea0ad
    Signed-off-by: gilles le brun <gil.lebrun@orange.com>

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -3810,6 +3810,7 @@ public class ComposeMessageActivity extends Activity
                     mDataSetChangedListener = new MessageListAdapter.OnDataSetChangedListener() {
         public void onDataSetChanged(MessageListAdapter adapter) {
             mPossiblePendingNotification = true;
+            checkPendingNotification();
         }
 
         public void onContentChanged(MessageListAdapter adapter) {
