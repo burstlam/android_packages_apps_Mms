69c1709fd90ca8045a59a708c96be9f1cf755268
burstlam <burstlam@gmail.com>
2013-03-14 14:05:30 +0800

    Revert "Add font size to Message threads"
    This reverts commit 3ff167b797563528391480252cf2680efe85aca9.
    
    Conflicts:
    	res/values/strings.xml
    
    Change-Id: I3ac12c704ccc5d087f17c24ca0a840a628b37bde

--- src/com/android/mms/ui/MessagingPreferenceActivity.java
+++ src/com/android/mms/ui/MessagingPreferenceActivity.java
@@ -104,14 +104,13 @@ public class MessagingPreferenceActivity extends PreferenceActivity
     public static final String QM_CLOSE_ALL_ENABLED      = "pref_key_close_all";
     public static final String QM_DARK_THEME_ENABLED     = "pref_dark_theme";
 
-    private static final String DIRECT_CALL_PREF         = "direct_call_pref";
-    public static final String MESSAGE_FONT_SIZE         = "pref_key_mms_message_font_size";
+    private static final String DIRECT_CALL_PREF = "direct_call_pref";
 
     // Mms Breath
     private static final String KEY_MMS_BREATH = "mms_breath";
 
     // Menu entries
-    private static final int MENU_RESTORE_DEFAULTS       = 1;
+    private static final int MENU_RESTORE_DEFAULTS    = 1;
 
     private Preference mSmsLimitPref;
     private Preference mSmsDeliveryReportPref;
