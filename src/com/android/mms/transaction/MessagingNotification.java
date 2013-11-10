b78572dd298d5387a6d18bc475a62828cd386f07
BigBrother1984 <stevewatersy@gmail.com>
2013-11-10 02:50:06 +0100

    Ditch halo entries according to aosp restart. Revert "taskon home"
    This reverts commit 1b4cb5d2ba4616061859a55ebb9e82c61fecb3ed.
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>

--- src/com/android/mms/transaction/MessagingNotification.java
+++ src/com/android/mms/transaction/MessagingNotification.java
@@ -894,7 +894,6 @@ public class MessagingNotification {
             }
         }
         TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(context);
-        taskStackBuilder.setTaskOnHome(false);
 
         // If we have more than one unique thread, change the title (which would
         // normally be the contact who sent the message) to a generic one that
@@ -1302,7 +1301,6 @@ public class MessagingNotification {
         }
 
         TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(context);
-        taskStackBuilder.setTaskOnHome(false);
         if (allFailedInSameThread) {
             failedIntent = new Intent(context, ComposeMessageActivity.class);
             if (isDownload) {
