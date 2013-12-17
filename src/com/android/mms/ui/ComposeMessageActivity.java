eefa7b5dbb5fe1a46f1eafa5a1deeb4695b59704
rdgate <rdgate@gmail.com>
2013-12-18 02:45:12 +0900

    No avatars option for speech bubbles theme
    Change-Id: I27c057ed1848550c7324ba518853ebec9ac457c5
    
    Conflicts:
    	src/com/android/mms/ui/ComposeMessageActivity.java

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -2001,7 +2001,6 @@ public class ComposeMessageActivity extends Activity
         mGestureSensitivity = prefs
                 .getInt(MessagingPreferenceActivity.GESTURE_SENSITIVITY_VALUE, 3);
         boolean showGesture = prefs.getBoolean(MessagingPreferenceActivity.SHOW_GESTURE, false);
-        boolean mSpeechBubbles = prefs.getBoolean(MessagingPreferenceActivity.SPEECH_BUBBLES, false);
         int unicodeStripping = prefs.getInt(MessagingPreferenceActivity.UNICODE_STRIPPING_VALUE,
                 MessagingPreferenceActivity.UNICODE_STRIPPING_LEAVE_INTACT);
         mInputMethod = Integer.parseInt(prefs.getString(MessagingPreferenceActivity.INPUT_TYPE,
@@ -2009,8 +2008,7 @@ public class ComposeMessageActivity extends Activity
 
         mLibrary = TemplateGesturesLibrary.getStore(this);
 
-        int layout = mSpeechBubbles ? R.layout.compose_message_activity_bubbles :
-                                      R.layout.compose_message_activity;
+        int layout = R.layout.compose_message_activity;
 
         GestureOverlayView gestureOverlayView = new GestureOverlayView(this);
         View inflate = getLayoutInflater().inflate(layout, null);
@@ -2322,6 +2320,9 @@ public class ComposeMessageActivity extends Activity
     protected void onRestart() {
         super.onRestart();
 
+        mMsgListAdapter = null;
+        initMessageList();
+
         // hide the compose panel to reduce jank when re-entering this activity.
         // if we don't hide it here, the compose panel will flash before the keyboard shows
         // (when keyboard is suppose to be shown).
@@ -2356,6 +2357,16 @@ public class ComposeMessageActivity extends Activity
         if (isSmsEnabled != mIsSmsEnabled) {
             mIsSmsEnabled = isSmsEnabled;
             invalidateOptionsMenu();
+        }
+
+        SharedPreferences prefs = PreferenceManager
+                .getDefaultSharedPreferences((Context) ComposeMessageActivity.this);
+        boolean mSpeechBubbles = prefs.getBoolean(MessagingPreferenceActivity.SPEECH_BUBBLES, false);
+
+        if (mSpeechBubbles) {
+            mMsgListView.setBackgroundColor(getResources().getColor(R.color.bubbles_bgcolor));
+        } else {
+            mMsgListView.setBackgroundColor(getResources().getColor(R.color.original_bgcolor));
         }
 
         initFocus();
